f19dda35e30b2c414853aa0193486775e4f8351f
era-070 <er865111@ucf.edu>
2026-07-18 13:07:44 -0400

    Add functionality to show/hide fun fact

---
+++ personal-bio-page/script.js
@@ -0,0 +1,16 @@
+// This code shows or hides the fun fact when the button is clicked.
+
+const button = document.getElementById("factButton");
+const funFact = document.getElementById("funFact");
+
+button.addEventListener("click", function () {
+
+    if (funFact.style.display === "none") {
+        funFact.style.display = "block";
+        button.textContent = "Hide Fun Fact";
+    } else {
+        funFact.style.display = "none";
+        button.textContent = "Show Fun Fact";
+    }
+
+});
